943bbcc333a2da8b6b91053ddbf5f824d9a04347
brijesheditor <brijesheditorhtw@gmail.com>
2025-02-25 00:27:13 +0530

    Update logs.pybk

--- logs.py
+++ logs.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @Oye_brijesh
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@bk_classes_28?si=hpJOu1O3DPqF3dTT
+# Ask Doubt on telegram @Oye_brijesh
 
 import logging
 from logging.handlers import RotatingFileHandler
